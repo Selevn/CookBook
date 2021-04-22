ae0bb2ccd9a23d8e24c499ad9a52e27c9dd3e330
Ivan Skorodumov <van000200136@gmail.com>
2021-04-22 16:10:08 +0300

    fix: CookCard switches moved in separate functions

--- cookbook/src/components/MultyUsed/CookCard/CookCardComponent.js
+++ cookbook/src/components/MultyUsed/CookCard/CookCardComponent.js
@@ -14,6 +14,41 @@ import { Views } from '../Views';
 import { Commented } from '../Commented';
 import { Liked } from '../Liked';
 
+const calculateSizes = (type) => {
+  const answer = {};
+  switch (type) {
+    case 'small': {
+      answer.width = '225px-(var(--padding-card)*2)';
+      answer.height = '360px-(var(--padding-card)*2)';
+      answer.imgHeight = '215px';
+      answer.imgWidth = '215px';
+      break;
+    }
+    case 'normal': {
+      answer.width = '310px';
+      answer.height = '395px';
+      break;
+    }
+    case 'bigImage': {
+      answer.width = 'calc(340px-(var(--padding-card)*2))';
+      answer.height = 'calc(361px-(var(--padding-card)*2))';
+      answer.imgHeight = '236px';
+      answer.imgWidth = '310px';
+      answer.showFooter = false;
+      break;
+    }
+    case 'long': {
+      answer.width = '215px';
+      answer.height = '215px';
+      break;
+    }
+    default: {
+      break;
+    }
+  }
+  return answer;
+};
+
 export const CookCardComponent = ({
   views,
   likes,
@@ -24,41 +59,7 @@ export const CookCardComponent = ({
   name,
   type,
 }) => {
-  let width;
-  let height;
-  let imgHeight;
-  let imgWidth;
-  let showFooter = true;
-  switch (type) {
-    case 'small': {
-      width = '225px-(var(--padding-card)*2)';
-      height = '360px-(var(--padding-card)*2)';
-      imgHeight = '215px';
-      imgWidth = '215px';
-      break;
-    }
-    case 'normal': {
-      width = '310px';
-      height = '395px';
-      break;
-    }
-    case 'bigImage': {
-      width = 'calc(340px-(var(--padding-card)*2))';
-      height = 'calc(361px-(var(--padding-card)*2))';
-      imgHeight = '236px';
-      imgWidth = '310px';
-      showFooter = false;
-      break;
-    }
-    case 'long': {
-      width = '215px';
-      height = '215px';
-      break;
-    }
-    default: {
-      break;
-    }
-  }
+  const { width, height, imgHeight, imgWidth, showFooter } = calculateSizes(type);
 
   return (
     <CookCardContainer vertical width={width} height={height}>
@@ -104,31 +105,36 @@ CookCardComponent.defaultProps = {
   type: 'small',
 };
 
-export const CookCardMenuComponent = ({ name, type }) => {
-  let width;
-  let height;
+const calculateImageSizes = (type) => {
+  const answer = {};
   switch (type) {
     case 'large': {
-      width = '540px';
-      height = '540px';
+      answer.width = '540px';
+      answer.height = '540px';
       break;
     }
     case 'long': {
-      width = '540px';
-      height = '255px';
+      answer.width = '540px';
+      answer.height = '255px';
       break;
     }
     case 'small': {
-      width = '255px';
-      height = '255px';
+      answer.width = '255px';
+      answer.height = '255px';
       break;
     }
     default: {
-      width = '255px';
-      height = '255px';
+      answer.width = '255px';
+      answer.height = '255px';
       break;
     }
   }
+  return answer;
+};
+
+export const CookCardMenuComponent = ({ name, type }) => {
+  const { width, height } = calculateImageSizes(type);
+
   return (
     <MinimizedCard image={CoolBook} containerHeight={height} containerWidth={width} type={type}>
       <MinimizedCardText type={type}>{name}</MinimizedCardText>
